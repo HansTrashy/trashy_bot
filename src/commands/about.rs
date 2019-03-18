4d1d5ea53dbfcfd891dab501e27b20d7dac8f8f7
Denis Richartz <denisrichartz92@gmail.com>
2019-03-18 11:08:34 +0100

    Github link added to about cmd

--- src/commands/about.rs
+++ src/commands/about.rs
@@ -1,5 +1,5 @@
 command!(about(_ctx, msg, _args) {
-    if let Err(why) = msg.channel_id.say("Der mülligste aller Bots! : )") {
+    if let Err(why) = msg.channel_id.say("Der mülligste aller Bots!\nGit: https://github.com/HansTrashy/trashy_bot") {
         println!("Error sending message: {:?}", why);
     }
 });
