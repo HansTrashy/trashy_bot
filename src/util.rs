9210ee6ca8ca25944174ac028baaeb35ade74eb8
Denis Richartz <denisrichartz92@gmail.com>
2019-03-20 23:21:09 +0100

    double equals in mod cmd list

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,7 @@
-static OTHER_MOD_CMD: [char; 4] = ['%', '=', '$', '='];
+static OTHER_MOD_CMD: [char; 3] = ['%', '=', '$'];
 
 pub fn sanitize_for_other_bot_commands(output: &str) -> String {
-    output
-        .chars()
-        .filter(|&c| !OTHER_MOD_CMD.contains(&c))
-        .collect::<String>()
+        output.chars()
+                .filter(|&c| !OTHER_MOD_CMD.contains(&c))
+                .collect::<String>()
 }
