da044e309614bc7c75f68c110a3faf4a18f37172
Denis Richartz <denisrichartz92@gmail.com>
2019-08-15 16:28:50 +0200

    humanize_duration util fn fixed and test added

--- src/util.rs
+++ src/util.rs
@@ -39,16 +39,39 @@ pub fn parse_duration(duration_str: &str) -> Option<Duration> {
 pub fn humanize_duration(duration: &Duration) -> String {
     let days = duration.num_days();
     let hours = duration.num_hours() - days * 24i64;
-    let minutes = duration.num_minutes() - hours * 60i64;
+    let minutes = duration.num_minutes() - (days * 24i64 * 60i64) - (hours * 60i64);
 
     match (days, hours, minutes) {
         (0, 0, 0) => "less than one minute".to_string(),
+        (0, 0, 1) => "1 minute".to_string(),
         (0, 0, x) => format!("{} minutes", x),
+        (0, 1, 0) => "1 hour".to_string(),
         (0, x, 0) => format!("{} hours", x),
         (0, x, y) => format!("{} hours {} minutes", x, y),
+        (1, 0, 0) => "1 day".to_string(),
         (x, 0, 0) => format!("{} days", x),
         (x, y, 0) => format!("{} days {} hours", x, y),
         (x, 0, y) => format!("{} days {} minutes", x, y),
         (x, y, z) => format!("{} days {} hours {} minutes", x, y, z),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{humanize_duration, Duration};
+
+    #[test]
+    fn check_humanized_duration() {
+        let duration = humanize_duration(&Duration::minutes(1440));
+
+        assert_eq!(duration, "1 day".to_string());
+
+        let duration = humanize_duration(&Duration::seconds(86400));
+
+        assert_eq!(duration, "1 day".to_string());
+
+        let duration = humanize_duration(&Duration::seconds(86399));
+
+        assert_eq!(duration, "23 hours 59 minutes".to_string());
     }
 }
