dfa0dc5e418a2d3544e4535f5627956a82fcc951
Denis Richartz <denisrichartz92@gmail.com>
2021-02-10 22:04:06 +0100

    improved roll command

--- src/commands/roll.rs
+++ src/commands/roll.rs
@@ -1,53 +1,117 @@
+use nom::{
+    branch::alt, bytes::complete::take_while, character::complete::char, character::is_digit,
+    combinator::opt, error::ErrorKind, multi::separated_list1, sequence::tuple, IResult,
+};
 use rand::prelude::*;
 use serenity::prelude::*;
 use serenity::{
     framework::standard::{macros::command, Args, CommandResult},
     model::channel::Message,
 };
-use tracing::error;
 
 #[command]
 #[description = "Roll some dice"]
 #[num_args(2)]
-#[usage = "*number_of_dice* *number_of_sides*"]
-#[example = "1 6"]
-#[example = "6 12"]
-async fn roll(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let amount_of_dice = args.single::<u64>()?;
-    let number_of_eyes = args.single::<u64>()?;
+#[usage = "*dice_str* *dice_str*"]
+#[example = "1d6"]
+#[example = "2d20-3"]
+async fn roll(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let dice_str = args.rest().trim();
 
-    if amount_of_dice > 50 {
-        return match msg
-            .channel_id
-            .say(&ctx.http, "Only <= 50 dice allowed")
-            .await
-        {
-            Ok(_) => Ok(()),
-            Err(e) => {
-                error!("Failure sending message: {:?}", e);
-                Err(e.into())
+    match parse_multiple_dice_str(dice_str.as_bytes()) {
+        Ok((_, dice)) => {
+            let mut total: isize = 0;
+            {
+                // dont hold rng over await points
+                let mut rng = rand::thread_rng();
+                for die in dice {
+                    let mut rolls = Vec::new();
+                    for _ in 0..die.number {
+                        rolls.push(rng.gen_range(1..=(die.sides as isize)));
+                    }
+                    total += rolls.iter().sum::<isize>() + die.flat;
+                }
             }
-        };
-    }
 
-    let mut dice_rolls = Vec::new();
-    {
-        // prevent rng to be held across an await point
-        let mut rng = rand::thread_rng();
-        for _ in 0..amount_of_dice {
-            dice_rolls.push(rng.gen_range(1..(number_of_eyes + 1)));
+            msg.reply(ctx, &format!("Your Roll: {}", total)).await?;
+        }
+        Err(_) => {
+            let _ = msg.reply(ctx, "Sorry that is not a valid die roll!").await;
         }
     }
 
-    match msg
-        .channel_id
-        .say(&ctx.http, &format!("Your roll: {:?}", dice_rolls))
-        .await
-    {
-        Ok(_) => Ok(()),
-        Err(e) => {
-            error!("Failure sending message: {:?}", e);
-            Err(e.into())
-        }
-    }
+    Ok(())
+}
+
+#[derive(Debug, PartialEq)]
+struct Die {
+    number: usize,
+    sides: usize,
+    flat: isize,
+}
+
+fn parse_flat_part(input: &[u8]) -> IResult<&[u8], Option<(char, &[u8])>> {
+    opt(tuple((alt((char('+'), char('-'))), take_while(is_digit))))(input)
+}
+
+fn parse_dice_str(input: &[u8]) -> IResult<&[u8], Die> {
+    let (input, number_digits) = take_while(is_digit)(input)?;
+    let (input, _) = char('d')(input)?;
+    let (input, side_digits) = take_while(is_digit)(input)?;
+    let (input, flat_part) = parse_flat_part(input)?;
+
+    let number = std::str::from_utf8(number_digits)
+        .map_err(|_| nom::Err::Error(nom::error::Error::new(input, ErrorKind::Char)))?
+        .parse::<usize>()
+        .map_err(|_| nom::Err::Error(nom::error::Error::new(input, ErrorKind::Char)))?;
+
+    let sides = std::str::from_utf8(side_digits)
+        .map_err(|_| nom::Err::Error(nom::error::Error::new(input, ErrorKind::Char)))?
+        .parse::<usize>()
+        .map_err(|_| nom::Err::Error(nom::error::Error::new(input, ErrorKind::Char)))?;
+
+    let flat = match flat_part {
+        Some((flat_sign, flat_digits)) => match flat_sign {
+            '+' => std::str::from_utf8(flat_digits)
+                .map_err(|_| nom::Err::Error(nom::error::Error::new(input, ErrorKind::Char)))?
+                .parse::<isize>()
+                .map_err(|_| nom::Err::Error(nom::error::Error::new(input, ErrorKind::Char)))?,
+            '-' => -std::str::from_utf8(flat_digits)
+                .map_err(|_| nom::Err::Error(nom::error::Error::new(input, ErrorKind::Char)))?
+                .parse::<isize>()
+                .map_err(|_| nom::Err::Error(nom::error::Error::new(input, ErrorKind::Char)))?,
+            _ => unreachable!("Reaching this means the nom parser failed"),
+        },
+        None => 0,
+    };
+
+    let die = Die {
+        number,
+        sides,
+        flat,
+    };
+
+    Ok((input, die))
+}
+
+fn parse_multiple_dice_str(input: &[u8]) -> IResult<&[u8], Vec<Die>> {
+    separated_list1(char(' '), parse_dice_str)(input)
+}
+
+#[test]
+fn test_nom_parser() {
+    let die_str = "1d6+2";
+
+    let (_, die) = parse_dice_str(die_str.as_bytes()).unwrap();
+
+    println!("{:?}", die);
+}
+
+#[test]
+fn test_nom_parser_multi() {
+    let die_str = "1d6+2 2d20-3";
+
+    let (_, die) = parse_multiple_dice_str(die_str.as_bytes()).unwrap();
+
+    println!("{:?}", die);
 }
